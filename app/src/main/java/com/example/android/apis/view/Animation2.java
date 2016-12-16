fc4c33a01e4c8732cfbd64773e711034f380551d
fengzhenhai <zhenhai.fzh@alibaba-inc.com>
2016-12-16 18:28:47 +0800

    文字链上下翻动效果

--- app/src/main/java/com/example/android/apis/view/Animation2.java
+++ app/src/main/java/com/example/android/apis/view/Animation2.java
@@ -22,12 +22,16 @@ import com.example.android.apis.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
+
+import java.util.zip.Inflater;
 
 
 public class Animation2 extends Activity implements
@@ -41,12 +45,30 @@ public class Animation2 extends Activity implements
         mFlipper = ((ViewFlipper) this.findViewById(R.id.flipper));
         mFlipper.startFlipping();
 
+        initViewFlipper2();
+
         Spinner s = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, mStrings);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s.setAdapter(adapter);
         s.setOnItemSelectedListener(this);
+    }
+
+    private void initViewFlipper2() {
+        mFlipper2 = (ViewFlipper) this.findViewById(R.id.flipper2);
+        String[] titles = {"可以传输MP3、视频、文档等文件",
+                "可让传输DCIM和Pictures文件夹下的照片", "可以与手机共享移动数据上网", "通过USB为手机充电"};
+        for(int i=0;i<titles.length;i++) {
+            View view = LayoutInflater.from(this).inflate(R.layout.animation_2_item, null);
+            ((TextView)view.findViewById(R.id.title)).setText(titles[i]);
+            mFlipper2.addView(view);
+        }
+        mFlipper2.setInAnimation(AnimationUtils.loadAnimation(this,
+                R.anim.push_up_in));
+        mFlipper2.setOutAnimation(AnimationUtils.loadAnimation(this,
+                R.anim.push_up_out));
+        mFlipper2.startFlipping();
     }
 
     public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
@@ -86,5 +108,6 @@ public class Animation2 extends Activity implements
             "Push up", "Push left", "Cross fade", "Hyperspace"};
 
     private ViewFlipper mFlipper;
+    private ViewFlipper mFlipper2;
 
 }
